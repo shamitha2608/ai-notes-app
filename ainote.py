d4fbcfce0b6b42eae2d4ac991441be4b40934d6a
shamitha2608 <shamita1290@gmail.com>
2026-06-12 20:01:39 +0530

    css

--- ainote.py
+++ ainote.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect, url_for
 app = Flask(__name__)
+notes=[]
 @app.route('/')
-def home():
-    return render_template("index.html")
+def index():
+    return render_template("index.html",notes = notes )
+@app.route("/add", methods=["POST"])
+def addnote():
+    note=request.form.get("note")
+    if note:
+        notes.append(note)
+    return redirect(url_for("index"))
+
 if __name__ == '__main__':    
     app.run(debug=True)
